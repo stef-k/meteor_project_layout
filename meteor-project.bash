94f6e65b55c69e804f8160208206f332ff042614
Stef Kariotidis <stef.kariotidis@gmail.com>
2014-09-11 20:55:07 +0300

    Template formatting

--- meteor-project.bash
+++ meteor-project.bash
@@ -40,36 +40,36 @@ HEAD="<head>
 
 # header.html
 HEADER="<template name=\"header\">
-<!--bootstrap related classes delete otherwise-->
-<div class=\"navbar navbar-default navbar-fixed-top\" role=\"navigation\">
-<div class=\"container\">
-<div class=\"navbar-header\">
-<button type=\"button\" class=\"navbar-toggle collapsed\" data-toggle=\"collapse\" data-target=\".navbar-collapse\">
-<span class=\"sr-only\">Toggle navigation</span>
-<span class=\"icon-bar\"></span>
-<span class=\"icon-bar\"></span>
-<span class=\"icon-bar\"></span>
-</button>
-<a class=\"navbar-brand\" >${PROJECT_NAME}</a>
-</div>
-<div class=\"collapse navbar-collapse\">
-<ul class=\"nav navbar-nav\">
-<li class=\"active\"><a href=\"{{pathFor 'index'}}\">Home</a></li>
-<li><a href=\"#about\">About</a></li>
-<!--{{> loginButtons}} if accounts-password meteor package is used-->
-</ul>
-</div><!--/.nav-collapse -->
-</div>
-</div>
+    <!--bootstrap related classes delete otherwise-->
+    <div class=\"navbar navbar-default navbar-fixed-top\" role=\"navigation\">
+        <div class=\"container\">
+            <div class=\"navbar-header\">
+                <button type=\"button\" class=\"navbar-toggle collapsed\" data-toggle=\"collapse\" data-target=\".navbar-collapse\">
+                    <span class=\"sr-only\">Toggle navigation</span>
+                    <span class=\"icon-bar\"></span>
+                    <span class=\"icon-bar\"></span>
+                    <span class=\"icon-bar\"></span>
+                </button>
+                <a class=\"navbar-brand\" >${PROJECT_NAME}</a>
+            </div>
+            <div class=\"collapse navbar-collapse\">
+                <ul class=\"nav navbar-nav\">
+                    <li class=\"active\"><a href=\"{{pathFor 'index'}}\">Home</a></li>
+                    <li><a href=\"#about\">About</a></li>
+                    <!--{{> loginButtons}} if accounts-password meteor package is used-->
+                </ul>
+            </div><!--/.nav-collapse -->
+        </div>
+    </div>
 </template>"
 
 FOOTER="<template name=\"footer\">
-<!--bootstrap footer classes delete otherwise-->
-<div class=\"footer\">
-<div class=\"container\">
-<p class=\"text-muted\">Place sticky footer content here.</p>
-</div>
-</div>
+    <!--bootstrap footer classes delete otherwise-->
+    <div class=\"footer\">
+        <div class=\"container\">
+            <p class=\"text-muted\">Place sticky footer content here.</p>
+        </div>
+    </div>
 </template>"
 
 STICKY_FOOTBAR="
